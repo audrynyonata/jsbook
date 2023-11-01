196c38d012b1e3f45d31c3b865119d066d18d3db
audrynyonata <audrynyonata@gmail.com>
2023-11-01 22:43:03 +0700

    lerna init

---
+++ packages/cli/src/commands/serve.ts
@@ -0,0 +1,39 @@
+import path from 'path';
+import { Command } from 'commander';
+import { serve } from 'local-api';
+
+interface LocalApiError {
+  code: string;
+}
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const serveCommand = new Command()
+  .command('serve [filename]') // optional args
+  .description('Open a file for editing')
+  .option('-p, --port <number>', 'port to run server on', '4005') // mandatory arg, ensure all param is string
+  .action(async (filename = 'notebook.js', options: { port: string }) => {
+    // console.log(process.cwd(), path.dirname(filename), path.basename(filename));
+
+    const isLocalApiError = (err: any): err is LocalApiError => {
+      return typeof err.code === 'string';
+    };
+
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      const basename = path.basename(filename);
+      await serve(parseInt(options.port), basename, dir, !isProduction);
+      console.log(`
+        Opened ${filename}. Navigate to http://localhost:${options.port} to see changes.
+      `);
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === 'EADDRINUSE') {
+          console.error('Port is in use. Try running on a different port.');
+        }
+      } else if (err instanceof Error) {
+        console.log("Here's the problem", err.message);
+      }
+      process.exit(1);
+    }
+  });
---
+++ packages/local-api/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
+  const app = express();
+
+  if (useProxy) {
+    // opt 1: react dev server running
+    // redirect/proxy to reflect changes in local dev instantly
+    app.use(
+      createProxyMiddleware({
+        target: 'http://127.0.0.1:3000',
+        ws: true, // websockets, used by cra in dev mode
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    // opt 2: react build/prod installed via cli
+    // for customers/end-user machine, serve files from installed local-client package
+    const packagePath = require.resolve('local-client/build/index.html'); // absolute path to index.html file
+    app.use(express.static(path.dirname(packagePath))); // serve the directory of that file
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
+};
